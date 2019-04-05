74f682c871ca08b04ad128fae71abe52a1e12feb
CooperLink <Cooper.r.link@gmail.com>
2019-04-05 14:02:37 -0400

    Created  a skill componenet and a difficulty selector

--- ReactSpaceTrader/screens/PlayerGenScreen.js
+++ ReactSpaceTrader/screens/PlayerGenScreen.js
@@ -9,25 +9,59 @@ import {
     View,
     Button,
     TextInput,
+    Picker
 } from 'react-native';
 
 class SkillComponent extends Component {
+    constructor(props){
+        super();
+        this.state = {
+            level: 0
+        }
+    }
     render() {
         return(
             <View style={style.skillBar}>
-                <Button
+                <Button style={{justifyContent: 'flex-start'}}
                     title = "-"
                     onPress={() => {
-                        this.props.navigation.navigate('Home');
+                        this.setState({level: this.state.level - 1});
                     }}
                 />
-                <Text> Skill Here </Text>
-                <Button
+                <Text> {this.props.skill} : {this.state.level} </Text>
+                <Button style={{justifyContent: 'flex-end',}}
                     title = "+"
                     onPress={() => {
-                        this.props.navigation.navigate('Home');
+                        this.setState({level: this.state.level + 1});
                     }}
                 />
+            </View>
+        );
+    }
+}
+
+class DifficultySelector extends Component {
+
+    constructor(props){
+        super();
+        this.state = {
+            currency: "Medium"
+        }
+
+    }
+    render(){
+        return(
+            <View>
+                <Text> Current Difficulty : {this.state.currency} </Text>
+                <Picker
+                    selectedValue={this.state.currency}
+                    style={{ height: 50, width: 100 }}
+                    onValueChange={(itemValue, itemIndex) => this.setState({currency: itemValue})}>
+                    <Picker.Item label = "Easy" value = "Easy"/>
+                    <Picker.Item label = "Medium" value = "Medium"/>
+                    <Picker.Item label = "Hard" value = "Hard"/>
+                    <Picker.Item label = "Impossible" value = "Impossible"/>
+                </Picker>
             </View>
         );
     }
@@ -49,7 +83,11 @@ export default class PlayerGenScreen extends Component {
                 />
 
                 <Text>Player Gen Screen</Text>
-                <SkillComponent/>
+                <SkillComponent skill = "Pilot"/>
+                <SkillComponent skill = "Fighter"/>
+                <SkillComponent skill = "Trader"/>
+                <SkillComponent skill = "Engineer"/>
+                <DifficultySelector/>
             </View>
         );
     }
@@ -60,15 +98,20 @@ const style = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent:'center'
+        justifyContent:'space-between',
+        flexDirection: 'column',
+        marginTop: 25,
+        marginBottom: 25
     },
     nameBar: {
         marginTop: 30
     },
     skillBar: {
         flexDirection: 'row',
-        flex: 1,
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        backgroundColor: '#aaa',
+        width: 300,
+        height: 30
 
     }
 });
