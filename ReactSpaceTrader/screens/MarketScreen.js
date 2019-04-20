638cddcfdbe9fccc1aaf0636ff4358d2b4c3187d
CooperLink <Cooper.r.link@gmail.com>
2019-04-20 18:58:43 -0400

    More market Stuff

--- ReactSpaceTrader/screens/MarketScreen.js
+++ ReactSpaceTrader/screens/MarketScreen.js
@@ -12,14 +12,18 @@ import {
     TextInput,
 } from 'react-native';
 import { db } from '../config';
-import items from '../constants/MarketItems';
+import {items} from '../constants/MarketItems';
 const { width, height } = Dimensions.get('window');
 
 let planetRef = (db.ref('Planets/curPlanet'));
 let planetName = "PlaceHolder";
+let planetResource, planetTech;
 planetRef.once('value', function(snapshot) {
     const planet = snapshot.val();
-    planetName = planet.attributes[0];
+    console.log(snapshot.toJSON());
+    planetName = planet["0"];
+    planetResource = planet["1"];
+    planetTech = planet["2"];
 });
 
 
@@ -34,14 +38,38 @@ creditsRef.on('value', function(snapshot) {
 export default class MarketScreen extends Component {
     constructor(props) {
         super()
+        this.validItems = this.generateValidItems();
         this.state = {
             credits: numCredits
         }
     }
+
+    generateValidItems() {
+        var validItems = [];
+        console.log(planetTech);
+        var planetTechNumber = techLevelDict[planetTech];
+        console.log('Check planet tech');
+        console.log(planetTechNumber);
+        for(var item in items) {
+            var itemArr = items[item];
+            if (itemArr[1] <= planetTechNumber) {
+                console.log(itemArr[0]);
+                validItems.push(itemArr);
+            }
+        }
+        return validItems;
+    }
+
     render() {
         return (
             <View style={style.marketStyle}>
                 <MarketHeader/>
+                <ScrollView>
+                    {
+                        
+                    }
+
+                </ScrollView>
 
 
             </View>
@@ -74,10 +102,43 @@ class MarketHeader extends Component {
     };
 }
 
+class MarketItem extends Component {
+    constructor() {
+        super();
+        this.state = {
+            owned: 0
+        }
+    }
+
+    render() {
+        return (
+            <View style = {style.marketItem}>
+                <Text> PRODUCT </Text>
+                <Text> PRICE </Text>
+                <Text> AMOUNT OWNED: {this.state.owned} </Text>
+                <Text> BUY </Text>
+                <Text>Sell</Text>
+            </View>
+        );
+    };
+}
+
+var techLevelDict = {
+    "PRE_AGRICULTURE" : 0,
+    "AGRICULTURE" : 1,
+    "MEDIEVAL" : 2,
+    "RENAISSANCE" : 3,
+    "EARLY_INDUSTRIAL" : 4,
+    "INDUSTRIAL" : 5,
+    "POST_INDUSTRIAL": 6,
+    "HI_TECH" : 7
+};
+
+let validItems = [];
 const style = StyleSheet.create({
     marketHeader: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#555',
         alignItems: 'center',
         flexDirection: 'column',
 
@@ -97,5 +158,16 @@ const style = StyleSheet.create({
         height: height,
         width:width,
         marginTop: 10,
+    },
+    marketItem: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#356',
+        width : '100%',
+        height: 20,
+
+    },
+    scroller: {
+
     }
 });
